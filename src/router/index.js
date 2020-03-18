5f51507f1597a4a6147e543906a7f6f41aadbe73
pengxuemei <xuemei.peng@sipingsoft.com>
2020-03-18 10:53:58 +0800

    后台管理系统的静态页面

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+let routes = [
+    {
+        path: '/',
+        component: ''
+    }
+]
+
+export default new Router({
+    routes: routes
+})
---
+++ src/views/infoManage/userManage/customerList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <el-table
+            :data="tableData"
+            border
+            style="width: 100%">
+            <el-table-column type='index'
+                            label="序号"
+                            width="180">
+            </el-table-column>
+            <el-table-column prop="usernme"
+                            label="用户名"
+                            width="180"> </el-table-column>
+            <el-table-column prop="createDate"
+                            label="创建日期"></el-table-column>
+            <el-table-column prop="location"
+                            label="创建定位"> </el-table-column>
+        </el-table>
+    </div>
+</template>
+
+<script>
+/**
+ * 用户列表
+ */
+export default {
+    data() {
+        return {
+            table: []
+        }
+    }
+}
+</script>
